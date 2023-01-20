eba456b7958077ed74794e3b700799decb72b324
pabloedu17 <pe.eduardo123@gmail.com>
2023-01-20 11:58:48 -0400

    identificando as letras do jogo da forca

--- forca.py
+++ forca.py
@@ -3,6 +3,21 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    enforcou = False
+    acertou = False
+
+    palavra_secreta = "banana"
+
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+
+        index =0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("Jogando...")
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
