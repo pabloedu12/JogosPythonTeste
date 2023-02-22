86aac288a7f889f0f640bbb5c8d55a9677a0b806
pabloedu17 <pe.eduardo123@gmail.com>
2023-02-21 22:23:32 -0400

    adicionando o arquivo com as palavras secretas

--- forca.py
+++ forca.py
@@ -1,9 +1,22 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "maça".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_certas = ['_' for letras in palavra_secreta]
 
     enforcou = False
