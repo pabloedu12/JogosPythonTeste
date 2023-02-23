d022a22ce8ee75b5eef1b1c8898b1a577df7ac09
pabloedu17 <pe.eduardo123@gmail.com>
2023-02-22 20:48:55 -0400

    criando mais funções

--- forca.py
+++ forca.py
@@ -3,41 +3,30 @@ import random
 def jogar():
     
     menu_de_jogo()
-
     palavra_secreta = carrega_palavras()
 
     letras_certas = inicializa_letras_certas(palavra_secreta)
+    print(letras_certas)
 
     enforcou = False
     acertou = False
-    erros = 0
-
-    print(letras_certas)
+    erros = 0    
 
     while(not enforcou and not acertou):
-        chute = input("Qual a letra? ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if(chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
-                    letras_certas[index] = letra
-                index += 1
+            marca_chute_correto(chute, letras_certas, palavra_secreta)
         else:
-            erros += 1
-            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
+            marca_chute_errado()
 
         enforcou = erros == 6
         acertou = '_' not in letras_certas 
-        print(letras_certas)
-
+        print(letras_certas)    
     if(acertou):
-        print("Voce ganhou!!")
+        imprime_mensagem_vencedor()
     else:
-        print("Voce perdeu!!")
-    
-    print("Fim de jogo")
+        imprime_mensagem_perdedor()        
 
 def inicializa_letras_certas(palavra):
     return ['_' for letras in palavra]
@@ -57,6 +46,28 @@ def carrega_palavras():
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
     return palavra_secreta
+
+def pede_chute():
+    chute = input("Qual a letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+def marca_chute_correto(chute, letras_certas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute.upper() == letra.upper()):
+            letras_certas[index] = letra
+        index += 1
+
+def imprime_mensagem_vencedor():
+    print("Voce ganhou!!")
+
+def imprime_mensagem_perdedor():
+    print("Voce perdeu!!")
+
+def marca_chute_errado():
+    erros += 1
+    print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
 
 if(__name__ == "__main__"):
     jogar()
